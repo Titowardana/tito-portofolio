919e3ff29f754012b4839d7c42e41d22adb97a2d
Titowardana <titowardana25@gmail.com>
2026-06-28 18:48:47 +0700

    fix: add Vercel Blob hostname to next/image remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -7,6 +7,14 @@ const nextConfig: NextConfig = {
     },
   },
   serverExternalPackages: ["@prisma/client"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*.public.blob.vercel-storage.com",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
